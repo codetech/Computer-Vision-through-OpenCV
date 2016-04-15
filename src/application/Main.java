1d34794ac9d11629e0db1180bbec1f2502ddfd88
Boris Nikulin <NikulinBE@gmail.com>
2016-04-14 17:19:12 -0700

    https://opencv-java-tutorials.readthedocs.org/en/latest/02-first-java-application-with-opencv.html
    Going through the tutorial
    
    OpenCV java works

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,44 @@
+
 package application;
-	
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+public class Main extends Application
+{
 
-public class Main extends Application {
+	static
+	{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			primaryStage.setScene (FXMLLoader.load (getClass ().getResource ("Webcam.fxml")));
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+	public void start (Stage primaryStage)
+	{
+		try
+		{
+			Parent root = FXMLLoader.load (getClass ().getResource ("Webcam.fxml"));
+			primaryStage.setScene (new Scene (root));
+			primaryStage.show ();
+			
+			Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+            System.out.println("mat = " + mat.dump());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace ();
 		}
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
+
+	public static void main (String[] args)
+	{
+		launch (args);
 	}
 }
