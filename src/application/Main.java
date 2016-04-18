0e57e6befb472871b672d7fd74ffd51efcbf0a91
Boris Nikulin <NikulinBE@gmail.com>
2016-04-17 18:25:59 -0700

    Hook up EyeOverlay

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -27,9 +28,12 @@ public class Main extends Application
 			Parent root = fxmlLoader.load ();
 			Controller controller = fxmlLoader.<Controller>getController ();
 			
+			EyeOverlay eyeReplacer = new EyeOverlay (controller.getOverlayCanvas (), new Image (getClass ().getResourceAsStream ("fire.png")));
+			controller.setEyeReplacer (eyeReplacer);
+			
 			primaryStage.setScene (new Scene (root));
 			primaryStage.show ();
-			primaryStage.setOnCloseRequest ((e) -> controller.stopCamera());
+			primaryStage.setOnCloseRequest ((e) -> {if (controller.isCameraActive ()) controller.stopCamera();});
 		}
 		catch (Exception e)
 		{
